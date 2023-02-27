37f0051f5e8d2cb2877b6d0988faa375964ce312
Kazunori Akiyama <kakiyama@mit.edu>
2023-02-27 13:58:09 -0500

    add docstrings -- no net changes

--- src/imagers/imager2D.jl
+++ src/imagers/imager2D.jl
@@ -8,6 +8,17 @@ struct Imager2D <: AbstractImager
     regularlizers
 end
 
+
+"""
+    ImagingProblem(imager::Imager2D, x0::AbstractArray)
+
+Initialize Optimization.OptimizationProblem for RML Imaging.
+Returns the initialized Optimization.OptimizationProblem object.
+
+# Arguments
+- `imager::Imager2D`: the imager
+- `x0::AbstractArray`: the initial image in the parameter domain.
+"""
 function ImagingProblem(imager::Imager2D, x0::AbstractArray)
     # define optimization function
     optfunc = OptimizationFunction(lossfunc, Optimization.AutoZygote())
@@ -18,6 +29,33 @@ function ImagingProblem(imager::Imager2D, x0::AbstractArray)
     return optprob
 end
 
+
+"""
+    ImagingProblem(imager::Imager2D, initialimage::AbstractEHTImage)
+
+Initialize Optimization.OptimizationProblem for RML Imaging.
+Returns the initialized Optimization.OptimizationProblem object.
+
+# Arguments
+- `imager::Imager2D`: the imager
+- `x0::AbstractArray`: the initial image in the parameter domain.
+"""
+function ImagingProblem(imager::Imager2D, initialimage::AbstractEHTImage)
+    x0 = initialize(imager.skymodel, image)
+    return ImagingProblem(imager::Imager2D, x0::AbstractArray)
+end
+
+
+"""
+    lossfunc(x, imager::Imager2D)
+
+The Loss function for the basic 2-dimensional RML Imaging problem.
+Returns the cost function.
+
+# Arguments
+- `x::AbstractArray`: the image parameters
+- `imager::Imager2D`: RML Imaging Setting
+"""
 function lossfunc(x, imager::Imager2D)
     # get the linear scale image
     x_linear = transform_linear_forward(imager.skymodel, x)
